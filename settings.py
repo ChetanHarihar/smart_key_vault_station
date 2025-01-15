4007dd734f8900aed9497f8c5530405d8b93dbcd
root <root@LAPTOP-061QP9LI>
2025-01-15 16:58:08 +0530

    modified settings

--- settings.py
+++ settings.py
@@ -1,6 +1,19 @@
 import customtkinter as ctk
-from urllib.parse import quote_plus
+from PIL import Image
+import os
+
 
 # Screen size
 WINDOW_WIDTH = 1024
 WINDOW_HEIGHT = 600
+
+# Device data
+STATION_NAME = os.environ.get('STATION_NAME')
+STATION_LINE = os.environ.get('STATION_LINE')
+STATION_REACH = os.environ.get('STATION_REACH')
+
+# Constants for database connection
+USERNAME = os.environ.get('DB_USERNAME')
+PASSWORD = os.environ.get('DB_PASSWORD')
+CLUSTER = os.environ.get('DB_CLUSTER')
+CONNECTION_STRING = f"mongodb+srv://{USERNAME}:{PASSWORD}@{CLUSTER}/"

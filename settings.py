e15f0b0534eb0a6f8388ee1f89ee1d7d1baacdcb
root <root@LAPTOP-061QP9LI>
2025-01-16 13:15:21 +0530

    modified settings

--- settings.py
+++ settings.py
@@ -7,6 +7,12 @@ import os
 WINDOW_WIDTH = 1024
 WINDOW_HEIGHT = 600
 
+# Colors
+purple = "#800080"
+white = "#FFFFFF"
+black = "#000000"
+green = "#36A211"
+
 # Device data
 STATION_NAME = os.environ.get('STATION_NAME')
 STATION_LINE = os.environ.get('STATION_LINE')
@@ -17,3 +23,19 @@ USERNAME = os.environ.get('DB_USERNAME')
 PASSWORD = os.environ.get('DB_PASSWORD')
 CLUSTER = os.environ.get('DB_CLUSTER')
 CONNECTION_STRING = f"mongodb+srv://{USERNAME}:{PASSWORD}@{CLUSTER}/"
+
+# Key data 
+s_and_t_ups = "S & T UPS"
+ser = "SER"
+ter= "TER"
+ass_and_tss= "ASS/TSS"
+dg = "DG"
+pump = "PUMP"
+
+# Key and department mapping
+KEY_MAP = {"Signalling": [s_and_t_ups, ser], 
+           "Telecom & AFC": [s_and_t_ups, ter], 
+           "Traction": [ass_and_tss], 
+           "E & M": [ass_and_tss, dg, pump], 
+           "all": [s_and_t_ups, ser, ter, ass_and_tss, dg, pump]
+           }

d091b817fdb9296853973a31507ce79352076fa2
chetan <chetanhs11@gmail.com>
2025-01-17 16:27:20 +0530

    created basic UI

--- maintainer_panel.py
+++ maintainer_panel.py
@@ -13,54 +13,60 @@ class MaintainerPanel(ctk.CTkFrame):
 
     def init_ui(self):
         # create top bar
-        topbar_frame = ctk.CTkFrame(master=self, width=WINDOW_WIDTH, height=50, fg_color=purple, corner_radius=0)
+        topbar_frame = ctk.CTkFrame(master=self, width=WINDOW_WIDTH, height=70, fg_color=purple, corner_radius=0)
         topbar_frame.pack_propagate(False)
         topbar_frame.pack()
 
         # label
-        ctk.CTkLabel(master=topbar_frame, text="Smart Key Vault", fg_color=purple, text_color=white, font=("Arial", 18)).pack(side='left', padx=15)
+        ctk.CTkLabel(master=topbar_frame, text="Smart Key Vault", fg_color=purple, text_color=white, font=("Arial", 22)).pack(side='left', padx=15)
 
         # exit button
         self.exit_image = Image.open(os.path.join('assets', 'exit.png'))  # Replace with your image file
         self.exit_image = self.exit_image.resize((30, 30))  # Resize the image to fit the button
-        self.exit_image_ctk = ctk.CTkImage(light_image=self.exit_image, dark_image=self.exit_image, size=(25, 20))
+        self.exit_image_ctk = ctk.CTkImage(light_image=self.exit_image, dark_image=self.exit_image, size=(30, 25))
 
-        self.exit_btn = ctk.CTkButton(master=topbar_frame, image=self.exit_image_ctk, text='', width=0, fg_color=purple, hover=False, command=lambda: print("Exit button clicked!"))
+        self.exit_btn = ctk.CTkButton(master=topbar_frame, image=self.exit_image_ctk, text='', width=0, fg_color=purple, hover=False, command=self.exit_panel)
         self.exit_btn.pack(side='right', padx=15)
 
         # info label
-        ctk.CTkLabel(master=self, text="Login Details", fg_color=white, text_color=green, font=("Arial", 20, 'bold')).pack(pady=(20, 10))
+        ctk.CTkLabel(master=self, text="Login Details", fg_color=white, text_color=green, font=("Arial", 34, 'bold')).pack(pady=(30, 0))
+
+        parent_frame = ctk.CTkFrame(master=self, fg_color=white, corner_radius=0)
+        parent_frame.pack(pady=20, fill='x')
 
         # Create a frame to display the maintainer details
-        self.maintainer_widget_frame = ctk.CTkFrame(master=self, fg_color=white)
-        self.maintainer_widget_frame.pack(pady=(0,10))
+        self.maintainer_widget_frame = ctk.CTkFrame(master=parent_frame, fg_color=white)
+        self.maintainer_widget_frame.pack(side='left', padx=(100,0))
 
         # name label
-        ctk.CTkLabel(master=self.maintainer_widget_frame, text="Name:", font=("Arial", 16)).grid(row=0, column=0, padx=5, pady=5)
+        ctk.CTkLabel(master=self.maintainer_widget_frame, text="Name:", font=("Arial", 24, 'bold')).grid(row=0, column=0, padx=5, pady=15, sticky='e')
         name_var = ctk.StringVar(value=self.maintainer_data['name'])
-        ctk.CTkEntry(master=self.maintainer_widget_frame, textvariable=name_var, text_color=black, font=("Arial", 16), width=250, justify="center", state="disabled").grid(row=0, column=1, padx=5, pady=5)
+        ctk.CTkEntry(master=self.maintainer_widget_frame, textvariable=name_var, text_color=black, font=("Arial", 22), width=350, height=40, justify="center", state="disabled").grid(row=0, column=1, padx=5, pady=15)
 
         # employee_id label
-        ctk.CTkLabel(master=self.maintainer_widget_frame, text="Employee ID:", font=("Arial", 16)).grid(row=1, column=0, padx=5, pady=5)
+        ctk.CTkLabel(master=self.maintainer_widget_frame, text="Employee ID:", font=("Arial", 24, 'bold')).grid(row=1, column=0, padx=5, pady=15, sticky='e')
         ID_var = ctk.StringVar(value=self.maintainer_data['employee_ID'])
-        ctk.CTkEntry(master=self.maintainer_widget_frame, textvariable=ID_var, text_color=black, font=("Arial", 16), width=250, justify="center", state="disabled").grid(row=1, column=1, padx=5, pady=5)
+        ctk.CTkEntry(master=self.maintainer_widget_frame, textvariable=ID_var, text_color=black, font=("Arial", 22), width=350, height=40, justify="center", state="disabled").grid(row=1, column=1, padx=5, pady=15)
 
         # department label
-        ctk.CTkLabel(master=self.maintainer_widget_frame, text="Department:", font=("Arial", 16)).grid(row=2, column=0, padx=5, pady=5)
+        ctk.CTkLabel(master=self.maintainer_widget_frame, text="Department:", font=("Arial", 24, 'bold')).grid(row=2, column=0, padx=5, pady=15, sticky='e')
         department_var = ctk.StringVar(value=self.maintainer_data['department'])
-        ctk.CTkEntry(master=self.maintainer_widget_frame, textvariable=department_var, text_color=black, font=("Arial", 16), width=250, justify="center", state="disabled").grid(row=2, column=1, padx=5, pady=5)
+        ctk.CTkEntry(master=self.maintainer_widget_frame, textvariable=department_var, text_color=black, font=("Arial", 22), width=350, height=40, justify="center", state="disabled").grid(row=2, column=1, padx=5, pady=15)
 
         # designation label
-        ctk.CTkLabel(master=self.maintainer_widget_frame, text="Designation:", font=("Arial", 16)).grid(row=3, column=0, padx=5, pady=5)
+        ctk.CTkLabel(master=self.maintainer_widget_frame, text="Designation:", font=("Arial", 24, 'bold')).grid(row=3, column=0, padx=5, pady=15, sticky='e')
         designation_var = ctk.StringVar(value=self.maintainer_data['designation'])
-        ctk.CTkEntry(master=self.maintainer_widget_frame, textvariable=designation_var, text_color=black, font=("Arial", 16), width=250, justify="center", state="disabled").grid(row=3, column=1, padx=5, pady=5)
+        ctk.CTkEntry(master=self.maintainer_widget_frame, textvariable=designation_var, text_color=black, font=("Arial", 22), width=350, height=40, justify="center", state="disabled").grid(row=3, column=1, padx=5, pady=15)
 
-        # frame to hold the key checkbox
-        self.key_checkbox_frame = ctk.CTkFrame(master=self, fg_color=white, corner_radius=0)
-        self.key_checkbox_frame.pack()
+        # user selection frame
+        self.selection_frame = ctk.CTkFrame(master=parent_frame, fg_color=white, corner_radius=0)
+        self.selection_frame.pack(side='right')
+
+        key_select_frame = ctk.CTkFrame(master=self.selection_frame, fg_color=white, corner_radius=0)
+        key_select_frame.pack(side='top')
 
         # select key label
-        ctk.CTkLabel(master=self.key_checkbox_frame, text="Select key:", font=("Arial", 16)).pack(pady=(0,5))
+        ctk.CTkLabel(master=key_select_frame, text="Select key:", font=("Arial", 24, 'bold')).grid(row=0, column=0, columnspan=2, padx=5, pady=15, sticky='nsew')
 
         # retrieve keys available for the department
         self.available_keys = KEY_MAP.get(self.maintainer_data['department'], [])
@@ -68,34 +74,63 @@ class MaintainerPanel(ctk.CTkFrame):
         # Dictionary to store BooleanVars for each checkbox state
         self.key_checkbox_vars = {}
 
-        for key in self.available_keys:
+        # Create a BooleanVar to store the state of the select all checkbox
+        self.key_checkbox_vars['Select all'] = ctk.BooleanVar()
+            
+        # Create a checkbox for each room and pack it
+        checkbox = ctk.CTkCheckBox(
+            master=key_select_frame,
+            text='Select all',
+            variable=self.key_checkbox_vars['Select all'],  # Linked to the checkbox state
+            onvalue=True,
+            offvalue=False,
+            font=("Arial", 26),
+            command=self.select_all_checkbox
+        )
+        checkbox.grid(row=1, column=0, padx=20, pady=15, sticky='w')
+
+        for index, key in enumerate(self.available_keys, start=2):
             # Create a BooleanVar to store the state of the checkbox
             self.key_checkbox_vars[key] = ctk.BooleanVar()
             
             # Create a checkbox for each room and pack it
             checkbox = ctk.CTkCheckBox(
-                master=self.key_checkbox_frame,
+                master=key_select_frame,
                 text=key,
                 variable=self.key_checkbox_vars[key],  # Linked to the checkbox state
                 onvalue=True,
                 offvalue=False,
-                font=("Arial", 14)
+                font=("Arial", 26)
             )
-            checkbox.pack(pady=4)
+            checkbox.grid(row=index, column=0, padx=20, pady=15, sticky='w')
 
-        # create a frame to hold the label and buttons
-        self.purpose_frame = ctk.CTkFrame(master=self, fg_color=white, corner_radius=0)
-        self.purpose_frame.pack(pady=(15,0))
+            ctk.CTkButton(master=key_select_frame, text="i", font=("Arial", 22), width=0, fg_color=purple, corner_radius=50, hover=None).grid(row=index, column=1, padx=20, pady=15, sticky='w')
+
+        # frame to hold purpose widgets
+        purpose_select_frame = ctk.CTkFrame(master=self.selection_frame, fg_color=white, corner_radius=0)
+        purpose_select_frame.pack(side='bottom', padx=50, pady=(20,0))
+
         # select purpose label
-        ctk.CTkLabel(master=self.purpose_frame, text="Select purpose:", font=("Arial", 16)).pack(side='left', padx=5)
+        ctk.CTkLabel(master=purpose_select_frame, text="Select purpose:", font=("Arial", 24, 'bold')).pack(side='left', padx=5)
         # maintainance button
-        ctk.CTkButton(master=self.purpose_frame, text="   Maintainance   ", font=("Arial", 14), width=0, fg_color=blue, hover=None, command=lambda:print("Purpose selected: Maintainance")).pack(side='left', padx=5)
+        ctk.CTkButton(master=purpose_select_frame, text="   Maintainance   ", font=("Arial", 22), width=0, height=40, fg_color=blue, hover=None, command=lambda:print("Purpose selected: Maintainance")).pack(side='left', padx=5)
         # emergency button
-        ctk.CTkButton(master=self.purpose_frame, text="   Emergency   ", font=("Arial", 14), width=0, fg_color=red, hover=None, command=lambda:print("Purpose selected: Emergency")).pack(side='left', padx=5)
+        ctk.CTkButton(master=purpose_select_frame, text="   Emergency   ", font=("Arial", 22), width=0, height=40, fg_color=red, hover=None, command=lambda:print("Purpose selected: Emergency")).pack(side='left', padx=5)
 
         # proceed button
-        self.proceed_button = ctk.CTkButton(master=self, text="   Proceed   ", font=("Arial", 14), width=0, fg_color=purple, hover=None, command=lambda:print("Proceed clicked!"))
-        self.proceed_button.pack(pady=15)
+        self.proceed_button = ctk.CTkButton(master=self, text="Proceed", font=("Arial", 24), width=180, height=50, fg_color=purple, hover=None, command=lambda:print("Proceed clicked!"))
+        self.proceed_button.pack(pady=(0,30))
+
+    def select_all_checkbox(self):
+        state = self.key_checkbox_vars['Select all'].get()
+        for key, key_var in self.key_checkbox_vars.items():
+            if key != 'Select all':  # Don't modify the 'Select all' checkbox itself
+                key_var.set(state)  # Set the state of the individual checkboxes
+
+
+    def exit_panel(self):
+        self.destroy()
+        root.destroy()
 
 
 if __name__ == "__main__":
@@ -103,6 +138,8 @@ if __name__ == "__main__":
 
     # Configure the root window
     root.geometry(f"{WINDOW_WIDTH}x{WINDOW_HEIGHT}")
+
+    root.overrideredirect(True)   # Enables full screen
 
     # Set CTk appearance mode
     ctk.set_appearance_mode("Light")
@@ -113,7 +150,7 @@ if __name__ == "__main__":
                         "employee_ID": "001",
                         "CSC": "13003297333",
                         "UID": "047F4BDA9C5A80",
-                        "department": "Signalling",
+                        "department": "Fire",
                         "designation": "Maintainer",
                         "contact_number": "9739090029",
                         "role": "maintainer",

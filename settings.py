d253102e9c12d4874ce158c116ea9289e8556857
root <root@LAPTOP-061QP9LI>
2025-01-19 22:31:38 +0530

    removed db connection string and added new colors

--- settings.py
+++ settings.py
@@ -17,17 +17,13 @@ red = "#FF0000"
 blue = "#0096FF"
 gray = "#A9A9A9"
 platinum = "#E5E4E2"
+lavender = "#E6E6FA"
+vl_green = "#D1FFBD"
 
 # Device data
 STATION_NAME = os.environ.get('STATION_NAME')
 STATION_LINE = os.environ.get('STATION_LINE')
 STATION_REACH = os.environ.get('STATION_REACH')
-
-# Constants for database connection
-USERNAME = os.environ.get('DB_USERNAME')
-PASSWORD = os.environ.get('DB_PASSWORD')
-CLUSTER = os.environ.get('DB_CLUSTER')
-CONNECTION_STRING = f"mongodb+srv://{USERNAME}:{PASSWORD}@{CLUSTER}/"
 
 # Key data 
 s_and_t_ups = "S & T UPS"

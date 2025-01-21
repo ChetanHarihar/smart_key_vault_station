b2b2dab428aa08eeea678745ec5458452e06f740
chetan <chetanhs11@gmail.com>
2025-01-21 11:24:30 +0530

    created login page GUI

--- login_panel.py
+++ login_panel.py
@@ -12,7 +12,15 @@ class LoginPanel(ctk.CTkFrame):
 
     def init_ui(self):
         # Load metro logo
-        pass
+        self.metro_image = Image.open(os.path.join('assets', 'namma_metro_logo.png'))  # Replace with your image file
+        self.metro_image = self.metro_image.resize((800, 350))  # Resize the image to fit the button
+        self.metro_image_ctk = ctk.CTkImage(light_image=self.metro_image, dark_image=self.metro_image, size=(800, 350))
+
+        # image label
+        ctk.CTkLabel(master=self, image=self.metro_image_ctk, text='').pack(pady=(180, 0))
+
+        # scan label
+        ctk.CTkLabel(master=self, text="Scan your ID card", text_color=green, font=("Arial", 34, 'bold')).pack(pady=(60, 0))
 
 
 if __name__ == "__main__":
@@ -21,9 +29,11 @@ if __name__ == "__main__":
     # Configure the root window
     root.geometry(f"{WINDOW_WIDTH}x{WINDOW_HEIGHT}")
 
+    root.overrideredirect(True)   # Enables full screen
+
     # Set CTk appearance mode
     ctk.set_appearance_mode("Light")
-    ctk.set_default_color_theme("themes/lavender.json")
+    ctk.set_default_color_theme("themes/violet.json")
 
     # Create instance of login panel
     frame = LoginPanel(master=root)

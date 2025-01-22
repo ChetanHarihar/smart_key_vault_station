28b27b42d8e803a6ffbc595d5d442de96f1f7cc6
chetan <chetanhs11@gmail.com>
2025-01-22 12:42:01 +0530

    added scan function

--- login_panel.py
+++ login_panel.py
@@ -1,10 +1,13 @@
 from settings import *
+from services.nfc_reader import read_nfc_tag
+import threading
 
 
 class LoginPanel(ctk.CTkFrame):
-    def __init__(self, master=None, **kwargs):
+    def __init__(self, master=None, update_panel_callback=None, **kwargs):
         super().__init__(master, **kwargs)
         self.root = master
+        self.update_panel_callback = update_panel_callback
         # Configure the frame dimensions and color
         self.configure(fg_color="white", width=WINDOW_WIDTH, height=WINDOW_HEIGHT)
         self.pack_propagate(False)
@@ -22,6 +25,29 @@ class LoginPanel(ctk.CTkFrame):
         # scan label
         ctk.CTkLabel(master=self, text="Scan your ID card", text_color=green, font=("Arial", 34, 'bold')).pack(pady=(60, 0))
 
+        # Start the RFID reading in a separate thread
+        self.read_nfc_thread = threading.Thread(target=self.read_nfc, daemon=True)
+        self.read_nfc_thread.start()
+
+    def read_nfc(self):
+        """
+        Continuously reads the RFID tags and updates the GUI with the UID.
+        Runs in a separate thread to prevent blocking the main GUI thread.
+        """
+        try:
+            # Read the UID from the NFC reader
+            uid = read_nfc_tag()
+            print(uid)
+            # load the panel
+            if self.update_panel_callback:
+                self.update_panel_callback(UID=uid)
+        except Exception as e:
+            print(f"Error reading RFID tag: {e}")
+        finally:
+            # Ensure the thread terminates after reading the tag
+            print("RFID reading thread finished.")
+            return  # Thread ends here
+
 
 if __name__ == "__main__":
     root = ctk.CTk()
@@ -29,7 +55,7 @@ if __name__ == "__main__":
     # Configure the root window
     root.geometry(f"{WINDOW_WIDTH}x{WINDOW_HEIGHT}")
 
-    root.overrideredirect(True)   # Enables full screen
+    # root.overrideredirect(True)   # Enables full screen
 
     # Set CTk appearance mode
     ctk.set_appearance_mode("Light")
